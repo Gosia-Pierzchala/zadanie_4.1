9ea251515b1bcd3e681e3ce93707126fcb66f6c4
Małgorzata Pierzchała <gk.kalinowska@gmail.com>
2018-07-16 10:42:46 +0200

    zadanie 4.1

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,15 @@
 public class Test {
     public static void main(String[] args) {
 
-        Triangle triangle1 = new Triangle(5, 1, 6);
+        Triangle triangle1 = new Triangle(8, 4, 5);
         TriangleCheck triCheck = new TriangleCheck();
-        boolean isRight = triCheck.isRight(triangle1);
 
-        if (triangle1.getSideA() >= triangle1.getSideC() || triangle1.getSideB() >= triangle1.getSideC()){
+        boolean finalCheck = triCheck.isRightTriangle(triangle1);
+
+        if (triangle1.getSideA() > triangle1.getSideC() || triangle1.getSideB() > triangle1.getSideC()){
             System.out.println("Bok C powinien być bokiem najdłuższym. Zaktualizuj parametry odpowiednio");
         } else {
-            System.out.println("Czy trójkąt jest prostokątny?\n" + isRight);
+            System.out.println("Czy trójkąt jest prostokątny?\n" + finalCheck);
         }
     }
 }
--- src/TriangleCheck.java
+++ src/TriangleCheck.java
@@ -1,5 +1,5 @@
 public class TriangleCheck {
-    public boolean isRight(Triangle triangle1){
+    public boolean isRightTriangle(Triangle triangle1){
         boolean result = (triangle1.getSideA() * triangle1.getSideA()) + (triangle1.getSideB() * triangle1.getSideB()) == triangle1.getSideC() * triangle1.getSideC();
         return result;
     }
